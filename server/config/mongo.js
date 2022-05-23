61bf59b64eadea0c998bf38dd45cd388986c71e0
revivocelulares <71577083+revivocelulares@users.noreply.github.com>
2022-05-23 02:17:12 -0300

    Final Commit

--- server/config/mongo.js
+++ server/config/mongo.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv';
 dotenv.config();
 
-const { MONGODB_URI } = process.env;
+const uri = process.env.MONGODB_URI;
 
-mongoose.connect(`${MONGODB_URI}`, {
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
